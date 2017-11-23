c3b12d863632e43683bf6d1739840dc359400448
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-11-23 21:45:25 +0000

    increment buttons for foods

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,92 @@
 $(() => {
+   $('.burgerAdd').click(function(e){
 
-  $('.burgerAdd').on('click', function (event){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#burger_quantity').val());
+         console.log(quantity);
+        // If is not undefined
 
-  });
+            $('#burger_quantity').val(quantity + 1);
+
+
+            // Increment
+
+    });
+
+     $('.burgurRemove').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#burger_quantity').val());
+
+        // If is not undefined
+
+            // Increment
+            if(quantity>0){
+            $('#burger_quantity').val(quantity - 1);
+            }
+    });
+
+        $('.friesAdd').click(function(e){
+
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#fries_quantity').val());
+         console.log(quantity);
+        // If is not undefined
+
+            $('#fries_quantity').val(quantity + 1);
+
+
+            // Increment
+
+    });
+
+     $('.friesRemove').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#fries_quantity').val());
+
+        // If is not undefined
+
+            // Increment
+            if(quantity>0){
+            $('#fries_quantity').val(quantity - 1);
+            }
+    });
+
+        $('.shakesAdd').click(function(e){
+
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#shakes_quantity').val());
+         console.log(quantity);
+        // If is not undefined
+
+            $('#shakes_quantity').val(quantity + 1);
+
+
+            // Increment
+
+    });
+
+     $('.shakesRemove').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#shakes_quantity').val());
+
+        // If is not undefined
+
+            // Increment
+            if(quantity>0){
+            $('#shakes_quantity').val(quantity - 1);
+            }
+    });
 
 });
