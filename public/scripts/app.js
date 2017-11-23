eb8ff2b317a114c2b7ec9fdfe3f3ee5113b84e8b
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-11-23 20:09:58 +0000

    quantities

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,7 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+
+  $('.burgerAdd').on('click', function (event){
+
+  });
+
 });
