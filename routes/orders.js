07b412b5e888c0176819191fc40778606e3b7550
kevin <kevin.rph.lee@gmail.com>
2017-11-25 23:27:16 +0000

    added more checks to see if order ID exists

--- routes/orders.js
+++ routes/orders.js
@@ -29,13 +29,19 @@ module.exports = (knex) => {
 
 
   router.get('/:id', (req, res) => {
-    knex.select('orders.id', 'menu_items.name', 'ordered_items.quantity', 'menu_items.price', 'orders.wait_time')
+    knex.select('orders.id', 'menu_items.name', 'ordered_items.quantity', 'menu_items.price', 'orders.wait_time', 'ordered_items.paid_price')
       .from('orders')
       .where('orders.id', req.params.id)
       .innerJoin('ordered_items', 'orders.id', 'ordered_items.order_id')
       .innerJoin('menu_items', 'menu_item_id', 'menu_items.id')
-      .then((result) => {
-        res.render('orderinfo', {order: result});
+      .then((results) => {
+        let totalPrice = 0;
+        for(let i = 0; i < results.length; i ++){
+          totalPrice += results[i]['paid_price'];
+        }
+        // console.log('Total Price: ' + totalPrice);
+        res.render('orderinfo', {order: result, totalPrice: totalPrice});
+        // res.json(results);
       });
   });
 
@@ -56,53 +62,75 @@ module.exports = (knex) => {
     console.log('burgers: ' + req.body.burgers);
     console.log('fries: ' + req.body.fries);
     console.log('shakes: ' + req.body.shakes);
-    knex
-      .insert({user_id: 1, wait_time: null})
-      .into('orders')
-      .returning('id')
-      .then(order_id => {
-        return new Promise((resolve, reject) => {
-          if(req.body.burgers > 0){
-            console.log('inserting burgers');
-            knex
-              .insert({order_id: Number(order_id), menu_item_id: 1, quantity: req.body.burgers, paid_price: 999})
-              .into('ordered_items')
-              .then(()=>{});
-          }
-          if(req.body.fries > 0){
-            console.log('inserting fries');
-            knex
-              .insert({order_id: Number(order_id), menu_item_id: 2, quantity: req.body.fries, paid_price: 999})
-              .into('ordered_items')
-              .then(()=>{});
-          }
-          if(req.body.shakes > 0){
-            console.log('inserting shakes');
-            knex
-              .insert({order_id: Number(order_id), menu_item_id: 3, quantity: req.body.shakes, paid_price: 999})
-              .into('ordered_items')
-              .then(()=>{});
-          }
-          resolve(order_id);
-        });
-      }).then((order_id) => {
-        console.log('about to send text message');
-        sendSMS(restaurantNumber, `Order ID ${order_id} Burgers ${req.body.burgers} Fries ${req.body.fries} Shakes ${req.body.shakes}`);
-        knex.select('phone_number')
-            .from('users')
-            .where('orders.id', Number(order_id))
-            .innerJoin('orders', 'users.id', 'orders.user_id')
-            .then((result) => {
-              const customerPhoneNumber = '+' + result[0]['phone_number'];
-              console.log('Phone number ' + customerPhoneNumber);
-              sendSMS(customerPhoneNumber, `Your order id is: ${order_id}`);
-            });
 
-        res.json(order_id);
-      })
-      .catch(error => {
-        res.status(500).json({ message: error.message });
+    knex
+      .select('price')
+      .from('menu_items')
+      .then((prices) =>{
+
+        // console.log("rohit");
+        // for(var i = 0; i<price.length; i++){
+        //   console.log("price :"+price[i].price);
+        // }
+        // console.log(JSON.stringify(price));
+        // const prices = JSON.stringify(price);
+        const burgersPrice = prices[0]['price'];
+        const friesPrice = prices[1]['price'];
+        const shakesPrice = prices[2]['price'];
+        const burgersQuantity = req.body.burgers;
+        const friesQuantity = req.body.fries;
+        const shakesQuantity = req.body.shakes;
+        // console.log('Prices: ' + burgerPrice, friesPrice, shakePrice);
+
+        knex
+          .insert({user_id: 1, wait_time: null})
+          .into('orders')
+          .returning('id')
+          .then(order_id => {
+            return new Promise((resolve, reject) => {
+              if(req.body.burgers > 0){
+                console.log('inserting burgers');
+                knex
+                  .insert({order_id: Number(order_id), menu_item_id: 1, quantity: burgersQuantity, paid_price: (burgersPrice * burgersQuantity)})
+                  .into('ordered_items')
+                  .then(()=>{});
+              }
+              if(req.body.fries > 0){
+                console.log('inserting fries');
+                knex
+                  .insert({order_id: Number(order_id), menu_item_id: 2, quantity: friesQuantity, paid_price: (friesPrice * friesQuantity)})
+                  .into('ordered_items')
+                  .then(()=>{});
+              }
+              if(req.body.shakes > 0){
+                console.log('inserting shakes');
+                knex
+                  .insert({order_id: Number(order_id), menu_item_id: 3, quantity: shakesQuantity, paid_price: (shakesPrice * shakesQuantity)})
+                  .into('ordered_items')
+                  .then(()=>{});
+              }
+              resolve(order_id);
+            });
+          }).then((order_id) => {
+            console.log('about to send text message');
+            sendSMS(restaurantNumber, `Order ID ${order_id} Burgers ${req.body.burgers} Fries ${req.body.fries} Shakes ${req.body.shakes}`);
+            knex.select('phone_number')
+                .from('users')
+                .where('orders.id', Number(order_id))
+                .innerJoin('orders', 'users.id', 'orders.user_id')
+                .then((result) => {
+                  const customerPhoneNumber = '+' + result[0]['phone_number'];
+                  // console.log('Phone number ' + customerPhoneNumber);
+                  sendSMS(customerPhoneNumber, `Your order id is: ${order_id}`);
+                });
+
+            res.json(order_id);
+          })
+          .catch(error => {
+            res.status(500).json({ message: error.message });
+          });
       });
+
   });
 
   /**
@@ -120,7 +148,6 @@ module.exports = (knex) => {
     let message = 'Default message';
 
 
-
     //Checking message format
     if( (body[1] === undefined) || (isNaN(body[0])) ){
       sendSMS(restaurantNumber, 'Incorrect message format: please send messages like <ORDER ID> <Wait time/Ready>');
@@ -133,10 +160,10 @@ module.exports = (knex) => {
       .from('orders')
       .then((result) =>{
         console.log("Result: ", result);
+        //Checks if the order exists in the DB
         if(result.length === 0){
           sendSMS(restaurantNumber, 'Error: Order does not exist');
         } else if (waitTime === 'ready'){
-          message = `Your order: ${orderID} is ready to pickup.`;
           console.log('Message is: ', message);
           knex('orders')
             .where('id', orderID)
@@ -149,11 +176,10 @@ module.exports = (knex) => {
                 .then((result) => {
                   const customerPhoneNumber = '+' + result[0]['phone_number'];
                   console.log('Phone number ' + customerPhoneNumber);
-                  sendSMS(customerPhoneNumber, message);
+                  sendSMS(customerPhoneNumber, `Your order: ${orderID} is ready to pickup.`);
                 });
             });
         } else {
-          message = `order id: ${orderID}, new approx. wait time: ${waitTime} minutes`;
           console.log('Message is: ', message);
           knex('orders')
             .where('id', orderID)
@@ -166,14 +192,12 @@ module.exports = (knex) => {
                 .then((result) => {
                   const customerPhoneNumber = '+' + result[0]['phone_number'];
                   console.log('Phone number ' + customerPhoneNumber);
-                  sendSMS(customerPhoneNumber, message);
+                  sendSMS(customerPhoneNumber, `order id: ${orderID}, new approx. wait time: ${waitTime} minutes`);
                 });
             });
           res.sendStatus(200);
         }
       })
-
-
 
   });
   return router;
