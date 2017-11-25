6a02a266e4e24a43a4cdbc29698e773c04f62913
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-11-25 19:47:59 +0000

    lint app.js file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,120 +1,62 @@
 
 
 $(() => {
-   $('.burgerAdd').click(function(e){
+  $('.burgerAdd').click(function(e) {
+    e.preventDefault();
+    var quantity = parseInt($('#burger_quantity').val());
+    $('#burger_quantity').val(quantity + 1);
+  });
 
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        var quantity = parseInt($('#burger_quantity').val());
-        // console.log(quantity);
-        // If is not undefined
+  $('.burgerRemove').click(function(e) {
+    e.preventDefault();
+    var quantity = parseInt($('#burger_quantity').val());
+    if(quantity > 0) {
+      $('#burger_quantity').val(quantity - 1);
+    }
+  });
 
-            $('#burger_quantity').val(quantity + 1);
+  $('.friesAdd').click(function(e) {
+    e.preventDefault();
+    var quantity = parseInt($('#fries_quantity').val());
+    $('#fries_quantity').val(quantity + 1);
+  });
 
+  $('.friesRemove').click(function(e) {
+    e.preventDefault();
+    var quantity = parseInt($('#fries_quantity').val());
+    if(quantity > 0) {
+      $('#fries_quantity').val(quantity - 1);
+    }
+  });
 
-            // Increment
+  $('.shakesAdd').click(function(e) {
+    e.preventDefault();
+    var quantity = parseInt($('#shakes_quantity').val());
+    $('#shakes_quantity').val(quantity + 1);
+  });
 
-    });
+  $('.shakesRemove').click(function(e) {
+    e.preventDefault();
+    var quantity = parseInt($('#shakes_quantity').val());
+    if(quantity > 0) {
+      $('#shakes_quantity').val(quantity - 1);
+    }
+  });
 
-     $('.burgurRemove').click(function(e){
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        var quantity = parseInt($('#burger_quantity').val());
-
-        // If is not undefined
-
-            // Increment
-            if(quantity>0){
-            $('#burger_quantity').val(quantity - 1);
-            }
-    });
-
-        $('.friesAdd').click(function(e){
-
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        var quantity = parseInt($('#fries_quantity').val());
-         //console.log(quantity);
-        // If is not undefined
-
-            $('#fries_quantity').val(quantity + 1);
-
-
-            // Increment
-
-    });
-
-     $('.friesRemove').click(function(e){
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        var quantity = parseInt($('#fries_quantity').val());
-
-        // If is not undefined
-
-            // Increment
-            if(quantity>0){
-            $('#fries_quantity').val(quantity - 1);
-            }
-    });
-
-        $('.shakesAdd').click(function(e){
-
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        var quantity = parseInt($('#shakes_quantity').val());
-         //console.log(quantity);
-        // If is not undefined
-
-            $('#shakes_quantity').val(quantity + 1);
-
-
-            // Increment
-
-    });
-
-     $('.shakesRemove').click(function(e){
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        var quantity = parseInt($('#shakes_quantity').val());
-
-        // If is not undefined
-
-            // Increment
-            if(quantity>0){
-            $('#shakes_quantity').val(quantity - 1);
-            }
-    });
-   /*  $.ajax({
-       url: '/orders',
-       method: 'POST',
-       data: $(this).serialize()
-       success: function(data) {
-       time: req.params.time,
-       user_id: req.params.user_id,
-       order_id:req.params.id
+  $('.submit-order').on('click', function(event) {
+    $.ajax({
+      method: "POST",
+      url: "/orders/",
+      data: {'burgers': $('#burger_quantity').val(),
+        'fries': $('#fries_quantity').val(),
+        'shakes': $('#shakes_quantity').val()
       }
-*/   $('.submit-order').on('click', function(event) {
-       $.ajax({
-        method: "POST",
-        url: "/orders/",
-        data: {'burgers': $('#burger_quantity').val(),
-               'fries': $('#fries_quantity').val(),
-               'shakes': $('#shakes_quantity').val()
-              }
-        })
-       $('#burger_quantity').val('0');
-       $('#fries_quantity').val('0');
-       $('#shakes_quantity').val('0');
-
-    })
-
-   });
+    });
+    $('#burger_quantity').val('0');
+    $('#fries_quantity').val('0');
+    $('#shakes_quantity').val('0');
+  });
+});
 
 
 
